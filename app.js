4a9745eaa80ab96c2aa74044295670bf003fdc69
mariana <mariana@gmail.com>
2026-09-07 10:25:29 -0500

    envio formularios, estructura body-parse, lectura e instalacion multer

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+const { error } = require('console');
 const express = require('express');
 const app = express();
 const port = process.env.MIPUERTO || 3003; 
+//libreria fs.path
+const sistemaArchivo = require("fs")
+const ruta = require("path")
+const rutaMiArchivo = ruta.join(__dirname,"datos.json")
 //middlewarc body-parse
 app.use(express.json())
+app.use(express.urlencoded({extended : true}))
 
 
 app.get('/', (req, res) => {
@@ -10,29 +16,29 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/aprendices', (req, res) => {
-res.status(200).json({Mensaje:"lista aprendices"})
+  sistemaArchivo.readFile(rutaMiArchivo, "utf-8", (error, Datos)=>{
+    if (error) res.status(500).json({error : "No se puede leer el archivo"})
+    const listaAprendices = JSON.parse (Datos)
+    res.status(200).json({ Listado : listaAprendices})
+  })
 });
+
 
 app.post('/api/aprendices', (req, res) => {
-  const datosAprendiz = req.body;
-  const edad = req.body.edad;
-
-  if (edad >= 18) {
-
-    return res.status(201).json({ 
-      'Mensaje': 'Eres mayor de edad', 
-      'Datos': datosAprendiz
-    });
-  }
-  else {
-
-    return res.status(201).json({ 
-      'Mensaje': 'Eres menor de edad', 
-      'Datos': datosAprendiz
-    });
-  }
-
+  const datosAprendiz = req.body
+ sistemaArchivo.readFile(rutaMiArchivo, "utf-8", (error, Datos)=>{
+    if (error) res.status(500).json({error : "No se puede leer el archivo"})
+    const listaAprendices = JSON.parse (Datos)
+  listaAprendices.push(datosAprendiz)
+  sistemaArchivo.writeFile(rutaMiArchivo, JSON.stringify(listaAprendices, null, 2), (error)=>{
+    if (error) res.status(500).json({error : "No se puede escribir en el archivo"})
+      res.status(200).json({Mensaje : "Creado", Datos: datosAprendiz})
+  })
+  
+  })
 });
+
+
 
 app.put('/api/aprendices/:id', (req, res) => {
 res.status(200).json({Mensaje:"actualiza aprendices"})
